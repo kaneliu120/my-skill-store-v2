d0626fb4772f33b9d46795607de83d2294e4b4af
kane <kane@kanedeMacBook-Pro.local>
2026-02-10 14:45:46 +0800

    chore: clear repository for new code upload

--- apps/api/src/orders/dto/create-order.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNumber, IsNotEmpty } from 'class-validator';
-
-export class CreateOrderDto {
-  @IsNumber()
-  @IsNotEmpty()
-  product_id: number;
-}
--- apps/api/src/payments/payments.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CryptoVerificationService } from './crypto-verification.service';
-
-@Module({
-  providers: [CryptoVerificationService],
-  exports: [CryptoVerificationService],
-})
-export class PaymentsModule {}
